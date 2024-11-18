a2a5981a6bbf74c3b341e823bf593927e0021596
ShotaArima <shota.arima@prtimes.co.jp>
2024-11-18 12:40:41 +0900

    upload installed smarty

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7a511960e687ae4e4f8706ac7695d4693dfd3be3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7a511960e687ae4e4f8706ac7695d4693dfd3be3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'smarty/smarty' => array(
+            'pretty_version' => 'v3.1.46',
+            'version' => '3.1.46.0',
+            'reference' => 'b3ade90dece67812410954528e0039fb5b73bcf7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../smarty/smarty',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
